18326a4df31df623655fcb5bcffd2569cd66e922
Quinn Lee <leekeunhwi@gmail.com>
2013-05-24 17:00:48 -0400

    created indiividual

--- lib/Individual.rb
+++ lib/Individual.rb
@@ -1,11 +1,22 @@
 class Individual
   attr_accessor :dna
+  attr_reader :fitness
 
   def initialize(genome_length)
+    @genome_length = genome_length
+    set_dna
   end
 
-  def make_dna(genome_length)
-    
+  def set_dna
+   @dna = (1..@genome_length).map{|gene| rand(2)}
+  end
+
+  def set_fitness(fitness_param)
+    @fitness = 0
+    dna.each_with_index do |gene,index|
+      @fitness +=1 if gene == fitness_param[index]
+    end
+    @fitness
   end
 
 end
--- spec/individual_spec.rb
+++ spec/individual_spec.rb
@@ -1,15 +1,53 @@
 require "spec_helper"
 
 describe Individual do
-  let(:genome_legth){10}
+  let(:genome_length){10}
+  let(:individual){Individual.new(genome_length)}
+
   
-  describe "#make DNA" do
+  describe "#set_DNA" do
+    let(:dna){individual.set_dna}
+
     it "has DNA" do
-      expect(individual.dna).to be_an_instance_of(Array)
-      individual.dna each do |gene|
+      expect(dna).to be_an_instance_of(Array)
+    end
+
+    it "Dna is a array of 0's or 1's" do
+      dna.each do |gene|
         expect([0,1]).to include(gene)
       end
     end
+
+    it "sets dna to length of genome_legth" do
+      expect(dna.length).to eql(genome_length)
+    end
+  end
+
+  describe "#set_fitness" do
+    let(:fitness_param){(1..genome_length).map{|gene| rand(2)}}
+    let(:fitness){individual.set_fitness(fitness_param)}
+    let(:dna){individual.set_dna}
+
+    it "sets a fitness score" do
+      expect(fitness).to be_an_instance_of(Fixnum)
+    end
+
+    it "is dependent on fitness_param" do
+      test_fitness = 0
+      dna.each_with_index do |gene,index|
+        test_fitness +=1 if gene == fitness_param[index]
+      end
+      expect(fitness).to eql(test_fitness)
+    end
+  end
+
+  describe "#new" do
+    let(:test_individual){Individual.new(genome_length)}
+
+    it "sets the dna"do
+      expect(test_individual.dna).to_not be_nil
+    end
+
   end
 
 end
